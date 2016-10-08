24b2c65cac07e9f319894ce4bd871c6be044a6c9
yberry <y_berry@hotmail.fr>
2016-10-08 20:56:22 +0200

    Beaucoup trop de trucs

--- Assets/Scripts/Flower/Flower.cs
+++ Assets/Scripts/Flower/Flower.cs
@@ -1,15 +1,37 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 [RequireComponent(typeof(Renderer))]
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class Flower : MonoBehaviour {
 
     private FlowerSpawnPoint point;
+    public bool IsFixed
+    {
+        get
+        {
+            return point != null;
+        }
+    }
+
     private Renderer ren;
+    private Collider2D col;
+    private Rigidbody2D rig;
+
+    private bool grabable = true;
+    public bool Grabable
+    {
+        get
+        {
+            return grabable;
+        }
+    }
 
 	// Use this for initialization
 	void Start () {
-	
+        ren = GetComponent<Renderer>();
+        col = GetComponent<Collider2D>();
+        rig = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
@@ -24,11 +46,33 @@ public class Flower : MonoBehaviour {
 
     public void Grab()
     {
-        if (point != null)
+        if (!grabable)
+        {
+            return;
+        }
+        if (IsFixed)
         {
             point.Free();
             point = null;
-            ren.enabled = false;
         }
+        ren.enabled = false;
+        col.enabled = false;
+        grabable = false;
+    }
+
+    public void Throw(bool underGod)
+    {
+        transform.SetParent(null);
+        ren.enabled = true;
+        grabable = !underGod;
+
+        float angle = Random.Range(Mathf.PI / 6f, 5f * Mathf.PI / 6f);
+        Vector2 velocity = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+        rig.velocity = velocity;
+    }
+
+    public void Launch()
+    {
+
     }
 }
--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -5,6 +5,7 @@ public class Player : MonoBehaviour {
 
     private const int maxFlowers = 5;
 
+    private List<Flower> triggers;
     private List<Flower> flowers;
     public bool complete
     {
@@ -14,26 +15,127 @@ public class Player : MonoBehaviour {
         }
     }
 
-    private bool underGod = false;
+    private bool underGod
+    {
+        get
+        {
+            return God.Get.IsUnderGodView(transform.position);
+        }
+    }
+    private bool grabing = false;
+    private bool attacked = false;
+    private bool attacking = false;
+    public bool IsAttacking
+    {
+        get
+        {
+            return attacking;
+        }
+    }
+    
 
 	// Use this for initialization
 	void Start () {
+        triggers = new List<Flower>();
         flowers = new List<Flower>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        underGod = God.Get.IsUnderGodView(transform.position);
+        if (grabing || attacked || attacking)
+        {
+            return;
+        }
+
+        if (Input.GetButtonDown("Grab"))
+        {
+            grabing = true;
+            if (underGod && flowers.Count > 0)
+            {
+                LaunchFlower();
+            }
+            else
+            {
+                foreach (Flower flower in triggers)
+                {
+                    GrabFlower(flower);
+                }
+                triggers.Clear();
+            }
+        }
+
+
 	}
 
-    public void GrabFlower(Flower flower)
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Flower")
+        {
+            Flower flower = col.GetComponent<Flower>();
+            if (flower.IsFixed)
+            {
+                triggers.Add(flower);
+            }
+            else
+            {
+                GrabFlower(flower);
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "Flower")
+        {
+            Flower flower = col.GetComponent<Flower>();
+            if (flower.IsFixed)
+            {
+                triggers.Remove(flower);
+            }
+        }
+    }
+
+    void GrabFlower(Flower flower)
     {
         if (complete)
         {
             return;
         }
+        flower.Grab();
         flowers.Add(flower);
         flower.transform.SetParent(transform);
         flower.transform.localPosition = Vector3.zero;
+    }
+
+    void Attack()
+    {
+        if (underGod)
+        {
+            Attacked(true);
+        }
+        else
+        {
+            attacking = true;
+        }
+    }
+
+    void Attacked(bool init)
+    {
+        attacked = true;
+        foreach (Flower flower in flowers)
+        {
+            flower.Throw(init);
+        }
+        flowers.Clear();
+    }
+
+    void LaunchFlower()
+    {
+        if (flowers.Count == 0)
+        {
+            return;
+        }
+        flowers[0].Launch();
+        flowers.RemoveAt(0);
     }
 }
