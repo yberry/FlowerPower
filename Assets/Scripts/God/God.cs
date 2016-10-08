1da9eec9e0acf4b281c5cb20dea9c1024403f2ac
yberry <y_berry@hotmail.fr>
2016-10-08 16:33:48 +0200

    God's commit

--- Assets/Scripts/God/God.cs
+++ Assets/Scripts/God/God.cs
@@ -3,13 +3,33 @@ using System.Collections;
 
 public class God : MonoBehaviour {
 
+    private static God instance;
+    public static God Get
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    public Light sight;
+
 	// Use this for initialization
 	void Start () {
-	
+        if (instance == null)
+        {
+            instance = this;
+        }
+        sight.type = LightType.Spot;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    public bool IsUnderGodView(Vector3 pos)
+    {
+        Vector3 sightPos = sight.transform.position;
+    }
 }
