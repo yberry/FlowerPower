738eda1c74e915e86a713c1b4c07230cbd44e611
yberry <y_berry@hotmail.fr>
2016-10-10 00:53:30 +0200

    FIN

---
+++ Assets/Scripts/Menu/Tuto.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Tuto : MonoBehaviour {
+
+    public Object scene;
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene(scene.name);
+        }
+	}
+}
