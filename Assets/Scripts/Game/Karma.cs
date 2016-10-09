f1a5ae78a09f74e339d031f7cc762a8ac6b8d2a4
yberry <y_berry@hotmail.fr>
2016-10-09 17:45:26 +0200

    Karma et coeurs

---
+++ Assets/Scripts/Game/Karma.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class Karma : MonoBehaviour {
+
+    private const int maxHearts = 10;
+
+    private static float[] amount
+    {
+        get
+        {
+            return new float[]
+            {
+                0f,
+                0.05f,
+                0.13f,
+                0.2f,
+                0.28f,
+                0.37f,
+                0.5f,
+                0.58f,
+                0.75f,
+                0.87f,
+                1f
+            };
+        }
+    }
+
+    private int nbHearts = 0;
+
+    private Image image;
+
+    [Tooltip("Vitesse de remplissage")]
+    public float fillSpeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+        image = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        image.fillAmount = Mathf.MoveTowards(image.fillAmount, amount[nbHearts], fillSpeed * Time.deltaTime);
+	}
+
+    public void AddHeart()
+    {
+        nbHearts++;
+    }
+}
